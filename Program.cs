f1a3ed60c264d9912b77f4d3822ca3a833b825e7
jyepe <yepejy@gmail.com>
2018-04-02 01:47:57 -0400

    Comments

--- Program.cs
+++ Program.cs
@@ -11,23 +11,28 @@ namespace LeftRotation
     {
         static void Main(string[] args)
         {
+            //Input array, k is number of rotations, n is length of array
             int[] a = new int[] { 1, 2, 3, 4, 5 };
             int k = 4;
             int n = 5;
 
+            //Make temp array length of number of rotations
             int[] temp = new int[k];
             int count = 0;
 
+            //Store the first k numbers in temp array
             for (int i = 0; i < k; i++)
             {
                 temp[i] = a[i];
             }
 
+            //Move each element in original array -k positions, starting at the kth position until end of array
             for (int i = k; i < n; i++)
             {
                 a[i - k] = a[i];
             }
 
+            //Insert temp values into original array
             for (int i = n - k; i < n; i++)
             {
                 a[i] = temp[count];
